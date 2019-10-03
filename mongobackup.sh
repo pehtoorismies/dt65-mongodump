792aa7e5c08446a76d1ba6ffa6f43435309bbc46
pehtoorismies <pehtoorismies@gmail.com>
2019-10-03 14:09:07 +0300

    Add cp-script

--- mongobackup.sh
+++ mongobackup.sh
@@ -8,15 +8,12 @@ timestamp=`date +%F-%H%M`
 # TAR_TARGET=$BACKUP_DIR/mongodb-$TIMESTAMP-$ENV_NS
 mongo_out_file=$backup_dir/mongodump-$HOST_NAME-$timestamp
 
-echo "Create dump"
+echo "Create dump & tar"
 mongodump --uri=$MONGO_URL -o $mongo_out_file
-echo "Create tar"
 tar cf $mongo_out_file.tar $mongo_out_file
 
-echo "$(ls $backup_dir)"
-
-echo 'init gsutil'
 echo $SERVICE_ACCOUNT_JSON > $backup_dir/service-account.json
 gcloud auth activate-service-account --key-file=$backup_dir/service-account.json
+gcutil cp $mongo_out_file.tar $GOOGLE_BUCKET
 
-echo "DONE"
+echo "Copied dump to Google storage"
