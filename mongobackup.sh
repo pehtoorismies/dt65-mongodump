0cf90cbac3a441132285b40919275b42e1b16ae2
pehtoorismies <pehtoorismies@gmail.com>
2019-10-03 13:47:50 +0300

    Add gsutils

--- mongobackup.sh
+++ mongobackup.sh
@@ -8,11 +8,15 @@ timestamp=`date +%F-%H%M`
 # TAR_TARGET=$BACKUP_DIR/mongodb-$TIMESTAMP-$ENV_NS
 mongo_out_file=$backup_dir/mongodump-$HOST_NAME-$timestamp
 
-
 echo "Create dump"
 mongodump --uri=$MONGO_URL -o $mongo_out_file
 echo "Create tar"
 tar cf $mongo_out_file.tar $mongo_out_file
 
 echo "$(ls $backup_dir)"
+
+echo 'init gsutil'
+echo $SERVICE_ACCOUNT_JSON > $backup_dir/service-account.json
+gcloud auth activate-service-account --key-file=$backup_dir/service-account.json
+
 echo "DONE"
